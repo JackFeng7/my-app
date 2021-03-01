13010173f6e4bc6e571be4f9509db2c29793211e
iaifeng <chinajackfeng7@aliyun.com>
2021-03-01 18:14:52 +0800

    init

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
+import { Card, Alert } from 'antd';
+import { useIntl } from 'umi';
+// import styles from './Welcome.less';
+
+export default () => {
+  const intl = useIntl();
+  return (
+    <PageContainer>
+      <Card>
+        <Alert
+          message={intl.formatMessage({
+            id: 'pages.welcome.alertMessage',
+            defaultMessage: '欢迎使用，已经发布。',
+          })}
+          type="success"
+          showIcon
+          banner
+          style={{
+            margin: -12,
+            marginBottom: 24,
+          }}
+        />
+        {/* <Typography.Text strong>
+          <FormattedMessage id="pages.welcome.advancedComponent" defaultMessage="高级表格" />{' '}
+          <a
+            href="https://procomponents.ant.design/components/table"
+            rel="noopener noreferrer"
+            target="__blank"
+          >
+            <FormattedMessage id="pages.welcome.link" defaultMessage="欢迎使用" />
+          </a>
+        </Typography.Text> */}
+        {/* <CodePreview>yarn add @ant-design/pro-table</CodePreview> */}
+        {/* <Typography.Text
+          strong
+          style={{
+            marginBottom: 12,
+          }}
+        >
+          <FormattedMessage id="pages.welcome.advancedLayout" defaultMessage="高级布局" />{' '}
+          <a
+            href="https://procomponents.ant.design/components/layout"
+            rel="noopener noreferrer"
+            target="__blank"
+          >
+            <FormattedMessage id="pages.welcome.link" defaultMessage="欢迎使用" />
+          </a>
+        </Typography.Text> */}
+        {/* <CodePreview>yarn add @ant-design/pro-layout</CodePreview> */}
+      </Card>
+    </PageContainer>
+  );
+};
